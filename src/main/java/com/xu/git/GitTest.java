bb1df0210bc7f3dee73bcf0607de44bc8e3cdf46
xu <2973905125@qq.com>
2022-09-27 21:47:42 +0800

    first commit

---
+++ src/main/java/com/xu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xu.git;
+
+/**
+ * @author 刘绪
+ * @description: TODO
+ * @date 2022/09/27 21:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
