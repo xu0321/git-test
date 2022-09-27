5f6376b31368647ec05c5fc2075b89cc32bbea34
xu <2973905125@qq.com>
2022-09-27 23:00:03 +0800

    push commit

--- src/main/java/com/xu/git/GitTest.java
+++ src/main/java/com/xu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git hot-fix");
         System.out.println("hello git master");
+        System.out.println("hello git master1");
     }
 }
