4e944f1b17945117afdb033b6b03ee73b7561504
xu <2973905125@qq.com>
2022-09-27 21:53:00 +0800

    third commit

--- src/main/java/com/xu/git/GitTest.java
+++ src/main/java/com/xu/git/GitTest.java
@@ -8,6 +8,7 @@ package com.xu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
